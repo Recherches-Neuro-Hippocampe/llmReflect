377514be55b23af8a27cdc4fdd026bf595905aec
alchemistwu <alchemistwu0521@gmail.com>
2023-06-30 15:01:25 -0400

    add push workflow

--- llmreflect/Tests/test_1.py
+++ llmreflect/Tests/test_1.py
@@ -1,8 +1,2 @@
-from llmreflect.Agents.EvaluationAgent import PostgressqlGradingAgent
-from llmreflect.Retriever.BasicRetriever import BasicEvaluationRetriever
-
-
-def test_postgres_grading_agent():
-    agent = PostgressqlGradingAgent()
-    retriever = BasicEvaluationRetriever()
-    agent.equip_retriever(retriever)
+def pseudo_test():
+    print("pseudo test")
