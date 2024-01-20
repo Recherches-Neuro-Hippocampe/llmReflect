7940533d98a38b3ee5b5a0efee038d1ae4baf9fd
EC2 Default User <ec2-user@ip-172-31-31-104.us-west-2.compute.internal>
2024-01-20 20:55:24 +0000

    vpc, github cannot connect to db

--- llmreflect/Tests/test_vb.py
+++ llmreflect/Tests/test_vb.py
@@ -1,7 +1,19 @@
 from llmreflect.Retriever.VectorDatabaseRetriever import \
     VectorDatabaseRetriever
+import pytest
+import os
 
 
+def in_workflow():
+    return os.getenv("GITHUB_ACTIONS")\
+        or os.getenv("TRAVIS") \
+        or os.getenv("CIRCLECI") \
+        or os.getenv("GITLAB_CI")
+
+
+@pytest.mark.skipif(bool(in_workflow()),
+                    reason="Only test database operations \
+                    in local env")
 def test_vb():
     search_engine = VectorDatabaseRetriever()
     question = "How do we recruit new patients?"
